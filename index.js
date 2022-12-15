0250856a96c29d7793a26e08c6160a83697c48e7
Himanshu <himanshupandey1036@gmail.com>
2022-12-15 19:40:50 +0530

    24hrs Digital Clock

---
+++ index.js
@@ -0,0 +1,13 @@
+alert('hello');
+const d = new Date();
+let h = d.getHours();
+let m = d.getMinutes();
+let s = d.getSeconds();
+
+console.log(h);
+console.log(m);
+console.log(s);
+
+document.getElementById('hour_val').innerText = h;
+document.getElementById('minutes_val').innerHTML = m;
+document.getElementById('second_val').innerHTML = s;

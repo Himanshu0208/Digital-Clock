eec41592ec1bf0ab278ae3a312a1537d61bc4e02
Himanshu <himanshupandey1036@gmail.com>
2022-12-15 20:05:51 +0530

    correcting index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-alert('hello');
+setInterval(() =>{
 const d = new Date();
 let h = d.getHours();
 let m = d.getMinutes();
@@ -11,3 +11,4 @@ console.log(s);
 document.getElementById('hour_val').innerText = h;
 document.getElementById('minutes_val').innerHTML = m;
 document.getElementById('second_val').innerHTML = s;
+},1000)

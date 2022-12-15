5bbecfa8c2d1f7d5dacb2e1bccd03c66d54f6a9f
Himanshu <98690271+Himanshu0208@users.noreply.github.com>
2022-12-15 21:11:10 +0530

    removing console log of h , m , and s

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ let h = d.getHours();
 let m = d.getMinutes();
 let s = d.getSeconds();
 
-console.log(h);
-console.log(m);
-console.log(s);
-
 document.getElementById('hour_val').innerText = h;
 document.getElementById('minutes_val').innerHTML = m;
 document.getElementById('second_val').innerHTML = s;
